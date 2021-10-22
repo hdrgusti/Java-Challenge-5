0cbdf9e9a475b9da78131b2610bb4cf81bc2087f
Hadhratin Agusti Susanto <91587930+hdrgusti@users.noreply.github.com>
2021-10-22 19:39:06 +0700

    Java Challenge

---
+++ Main.java
@@ -0,0 +1,37 @@
+class Main {
+public static void main(String[] args) {
+    Heroes hero = new Heroes();
+
+    hero.setProfile("Hadhratin Agusti Susanto", 21, 174);
+    hero.profile();
+    
+
+    // jalankan aktivitas disini...
+
+    // contoh:
+    hero.jalan();
+    hero.jalan();
+    hero.jalan();
+    hero.jalan();
+    hero.jalan();
+
+    hero.lari();
+    hero.lari();
+
+    hero.makan();
+    hero.makan();
+    hero.makan();
+
+    hero.minum();
+
+    hero.lompat();
+    hero.lompat();
+    hero.lompat();
+
+    hero.duduk();
+
+    // tampilkan status terkini...
+    hero.profile();
+    
+  }
+}
